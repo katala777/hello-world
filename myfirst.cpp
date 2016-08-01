5f7472ff1dac0941b6ecf2193e52f7263735febb
Иван <im142@mail.ru>
2016-08-01 14:40:34 +0300

    myfirst.cpp has been added

---
+++ myfirst.cpp
@@ -0,0 +1,11 @@
+//myfirst.cpp -- выводит сообщение на экран
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout<<"Come up and C++ me some time.";
+	cout<<endl;
+	cout<<"You won't regregt it"<<endl;
+	return 0;
+}
