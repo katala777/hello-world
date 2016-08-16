a85d15cb4c2eba04c6551e2543ede151d374f6fd
Иван <im142@mail.ru>
2016-08-16 18:30:20 +0300

    limits.cpp has been added

---
+++ chapter-3/examples/limits.cpp
@@ -0,0 +1,20 @@
+﻿//limits.cpp -- ��������� ����������� ����� �����
+#include <iostream>
+#include <climits>
+
+int main()
+{
+	setlocale(LC_ALL,"");
+	using namespace std;
+	int n_int = INT_MAX;
+	short n_short = SHRT_MAX;
+	long n_long = LONG_MAX;
+	long long n_llong = LLONG_MAX;
+	
+	cout << "int is " << sizeof (int) << " байт." << endl;
+	cout << "short is " << sizeof n_short << " bytes." << endl;
+	cout << "long is " << sizeof n_long << " bytes." << endl;
+	cout << "long long is " << sizeof n_llong << " bytes." << endl;
+	return 0;
+}
+
