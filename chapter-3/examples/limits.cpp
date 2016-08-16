b3d01a1af1ddfeec7b2b3a153e2513a01e2477dc
Иван <im142@mail.ru>
2016-08-16 18:42:52 +0300

    limits.cpp был закончен

--- chapter-3/examples/limits.cpp
+++ chapter-3/examples/limits.cpp
@@ -1,20 +1,30 @@
-п»ї//limits.cpp -- пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+//limits.cpp -- некоторые ограничения целых чисел
 #include <iostream>
 #include <climits>
 
 int main()
 {
 	setlocale(LC_ALL,"");
-	using namespace std;
+	using namespace std;//использовать пространство имен std
 	int n_int = INT_MAX;
 	short n_short = SHRT_MAX;
 	long n_long = LONG_MAX;
 	long long n_llong = LLONG_MAX;
 	
-	cout << "int is " << sizeof (int) << " Р±Р°Р№С‚." << endl;
+	//операция sizeof выдает размер типа или переменной
+	cout << "int is " << sizeof (int) << " bytes." << endl;
 	cout << "short is " << sizeof n_short << " bytes." << endl;
 	cout << "long is " << sizeof n_long << " bytes." << endl;
-	cout << "long long is " << sizeof n_llong << " bytes." << endl;
+	cout << "long long is " << sizeof n_llong << " bytes." << endl << endl;
+	
+	cout << "Maximum values:" << endl;
+	cout << "int: " << n_int << endl;
+	cout << "short: " << n_short << endl;
+	cout << "long: " << n_long << endl;
+	cout << "long long: " << n_llong << endl << endl;
+	
+	cout << "Minimum int value = " << INT_MIN << endl;
+	cout << "Bits per byte = " << CHAR_BIT << endl;
 	return 0;
 }
 
