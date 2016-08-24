2a8290372acab3785c68f9035f70a96bbd74064b
Иван <im142@mail.ru>
2016-08-24 21:50:49 +0300

    Добавил листинги 3.11 - 3.14

---
+++ chapter-3/examples/modulus.cpp
@@ -0,0 +1,18 @@
+﻿//modulus.cpp -- использует операцию % для преобразования фунтов в стоуны
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	const int Lbs_per_stn = 14;		// 1 стоун равен 14 фунтам
+	int lbs;
+	
+	cout << "Enter your weight in pounds: ";
+	cin >> lbs;
+	int stone = lbs / Lbs_per_stn;	// количество целых стоунов
+	int pounds = lbs % Lbs_per_stn;	// количество фунтов
+	cout << lbs << " pounds are " << stone
+		 << " stone, " << pounds << " pound(s)." << endl;
+	return 0;
+}
+
