3de798f63a057f41eb6948f56286b951f0b98d8a
Иван <im142@mail.ru>
2016-09-16 12:54:25 +0300

    Добавил листинги 4.2 - 4.6

---
+++ chapter-4/examples/instr3.cpp
@@ -0,0 +1,18 @@
+﻿// instr3.cpp -- чтение более одного слова с помощью get()
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	const int ArSize = 20;
+	char name[ArSize];
+	char dessert[ArSize];
+	
+	cout << "Enter your name:\n";
+	cin.get(name, ArSize).get();						// читать строку и символ новой строки
+	cout << "Enter your favorite dessert:\n";
+	cin.get(dessert, ArSize).get();
+	cout << "I have some delicious " << dessert;
+	cout << " for you, " << name << ".\n";
+	return 0;
+}
---
+++ chapter-4/examples/strings.cpp
@@ -0,0 +1,24 @@
+﻿// strings.cpp -- сохранение строк в массиве
+#include <iostream>
+#include <cstring>							// для функции strlen()
+
+int main()
+{
+	using namespace std;
+	const int Size = 15;
+	char name1[Size];						// пустой массив
+	char name2[Size] = "C++owboy";			// инициализация массива
+	
+	cout << "Howdy! I'm " << name2;
+	cout << "! What's your name?\n";
+	cin >> name1;
+	cout << "Well, " << name1 << ", your name has ";
+	cout << strlen(name1) << " letters and is stored\n";
+	cout << "in array of " << sizeof(name1) << " bytes.\n";
+	cout << "Your initial is " << name1[0] << ".\n";
+	name2[3] = '\0';						// установка нулевого символа
+	cout << "Here are first 3 characters of my name: ";
+	cout << name2 << endl;
+	return 0;
+}
+
