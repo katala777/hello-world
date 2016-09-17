f0c78052381d62e98a8b6ac5a82a5087fc54c616
Иван <im142@mail.ru>
2016-09-17 16:42:30 +0300

    Добавил листинги 4.7 - 4.9

---
+++ chapter-4/examples/strtype1.cpp
@@ -0,0 +1,26 @@
+﻿// strtype1.cpp -- использование класса C++ string
+#include <iostream>
+#include <string>
+
+int main()
+{
+	using namespace std;
+	char charr1[20];
+	char charr2[20] = "juguar";
+	string str1;
+	string str2 = "panther";
+	
+	cout << "Enter a kind of feline: ";			// Введите животное из семейства кошачьих
+	cin >> charr1;
+	cout << "Enter another kind of feline: ";	// Введите другое животное из семейства кошачьих
+	cin >> str1;
+	cout << "Here are some felines:\n";
+	cout << charr1 << ", " << charr2 << ", "
+		<< str1 << ", " << str2 << endl;
+	cout << "The third letter in " << charr2 << " is "
+		<< charr2[2] << endl;
+	cout << "The third letter in " << str2 << " is "
+		<< str2[2] << endl;
+	return 0;
+}
+
