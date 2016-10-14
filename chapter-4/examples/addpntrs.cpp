9aa3d915e0c02f5957da1d8d28c375fbf937ddaf
Иван <im142@mail.ru>
2016-10-15 01:03:36 +0300

    Добавил листинги 4.18 - 4.20

---
+++ chapter-4/examples/addpntrs.cpp
@@ -0,0 +1,35 @@
+﻿// addpntrs.cpp -- сложение указателей
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	double wages[3] = {10000.0, 20000.0, 30000.0};
+	short stacks[3] = {3, 2, 1};
+	
+	// Два способа получить адрес массива
+	double *pw = wages;						// имя массива равно адресу
+	short *ps = &stacks[0];					// либо с использованием операции взятия адреса с элементов массива
+	cout << "pw = " << pw << ", *pw = " << *pw << endl;
+	pw = pw + 1;
+	cout << "add 1 to the pw pointer:\n";
+	cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+	cout << "ps = " << ps << ", *ps = " << *ps << endl;
+	ps = ps + 1;
+	cout << "add 1 to the ps pointer:\n";
+	cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
+	
+	// Доступ к двум элементам с помощью нотации массивов
+	cout << "access two elements with array notation\n";
+	cout << "stacks[0] = " << stacks[0]
+		 << ", stacks[1] = " << stacks[1] << endl;
+		 
+	// Доступ к двум элементам с помощью нотации указателей
+	cout << "access two elements with pointer notation\n";
+	cout << "*stacks = " << *stacks
+		 << ", *(stacks + 1) = " << *(stacks + 1) << endl;
+	cout << sizeof(wages) << " = size of wages array\n";
+	cout << sizeof(pw) << " = size of pointer\n";
+	return 0;
+}
+
---
+++ chapter-4/examples/ptrstr.cpp
@@ -0,0 +1,33 @@
+﻿// ptrstr.cpp -- использование указателей на строки
+#include <iostream>
+#include <cstring>	// объявление strlen(), strcpy()
+
+int main()
+{
+	using namespace std;
+	char animal[20] = "bear";
+	const char *bird = "wren";
+	char *ps;
+	cout << animal << " and ";
+	cout << bird << "\n";
+	// cout << ps << "\n";			// может отобразить мусор, но может вызвать и аварийное завершение программы
+	
+	cout << "Enter a kind of animal: ";
+	cin >> animal;					// нормально, если вводится меньше 20 символов
+	// cin >> ps; очень опасная ошибка, чтобы пробовать;
+	// ps не указывает на выделенное пространство
+	
+	ps = animal;
+	cout << ps << "!\n";
+	cout << "Before using strcpy(): \n";
+	cout << animal << " at " << (int *)animal << endl;
+	cout << ps << " at " << (int *)ps << endl;
+	ps = new char[strlen(animal) + 1];
+	strcpy(ps, animal);
+	cout << "After using strcpy(): \n";
+	cout << animal << " at " << (int *)animal << endl;
+	cout << ps << " at " << (int *)ps << endl;
+	delete [] ps;
+	return 0;
+}
+
